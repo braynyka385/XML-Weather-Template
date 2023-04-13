4e2d1107b008754a880276e1c3e0f249d18c64c4
Brayden Nykamp <braynyka385@outlook.com>
2023-04-13 10:47:23 -0400

    fixed

--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -33,16 +33,17 @@ namespace XMLWeather
         private void ExtractForecast()
         {
             XmlReader reader = XmlReader.Create("http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0");
+            reader.ReadToFollowing("timezone");
+            string timeshiftS = reader.ReadString();
+            int timeshift = 0;
+            if (timeshiftS != "")
+                timeshift = int.Parse(timeshiftS);
 
             while (reader.Read())
             {
                 //create a day object
                 Day d = new Day();
-                reader.ReadToFollowing("timezone");
-                string timeshiftS = reader.ReadString();
-                int timeshift = 0;
-                if(timeshiftS != "")
-                     timeshift = int.Parse(timeshiftS);
+                
                 //fill day object with required data
                 reader.ReadToFollowing("time");
                 d.date = reader.GetAttribute("day");
@@ -83,7 +84,7 @@ namespace XMLWeather
 
                 if(d.date != null)
                     days.Add(d);
-                //TODO: if day object not null add to the days list
+                //if day object not null add to the days list
             }
         }
 
